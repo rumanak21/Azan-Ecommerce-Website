0f3c5ed547dc5268c4077d9ef054815ad4cf6509
rumanak21 <rumana2k20@gmail.com>
2024-02-02 21:18:02 +0100

    added cart and sign in btn

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,8 +1,23 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <nav>
+        <div className='navbar justify-between bg-base-300'>
+        <Link href="/" className='btn btn-ghost text-lg'>Azan</Link>
+        <ul className='flex'>
+          <li>
+            <Link href="/cart" className='btn btn-ghost rounded-btn '>Cart</Link>
+          </li>
+          <li>
+            <Link href="/signin" className='btn btn-ghost rounded-btn '>Sign in</Link>
+          </li>
+        </ul>
+        </div>
+      </nav>
+    </header>
   )
 }
 
