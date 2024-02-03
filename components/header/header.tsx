062a5a596b7474bdfbc6326540b044ec5bfd16b5
rumanak21 <rumana2k20@gmail.com>
2024-02-03 12:47:29 +0100

    add to cart functionality

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link'
 import React from 'react'
+import Menu from './Menu'
 
 const Header = () => {
   return (
@@ -7,14 +8,7 @@ const Header = () => {
       <nav>
         <div className='navbar justify-between bg-base-300'>
         <Link href="/" className='btn btn-ghost text-lg'>Azan</Link>
-        <ul className='flex'>
-          <li>
-            <Link href="/cart" className='btn btn-ghost rounded-btn '>Cart</Link>
-          </li>
-          <li>
-            <Link href="/signin" className='btn btn-ghost rounded-btn '>Sign in</Link>
-          </li>
-        </ul>
+        <Menu/>
         </div>
       </nav>
     </header>
