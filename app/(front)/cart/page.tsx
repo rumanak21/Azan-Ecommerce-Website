7d6f81fd3c0bcb29dcc500b500775e2d296278ea
rumanak21 <rumana2k20@gmail.com>
2024-02-04 16:52:27 +0100

    Cart page functionality and design added

---
+++ app/(front)/cart/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import CartDetails from './CartDetails'
+
+export const metadata = {
+    title: 'Shopping Cart'
+}
+const CartPage = () => {
+  return (
+    <CartDetails/>
+  )
+}
+
+export default CartPage
